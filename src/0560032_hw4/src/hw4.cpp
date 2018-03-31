863a66eb133aba979d808b22d422dee2ac2174ab
Kevin83825 <kevin83825.ece05g@g2.nctu.edu.tw>
2018-03-31 10:03:00 +0800

    push hw4

---
+++ src/0560032_hw4/src/hw4.cpp
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <math.h>
+
+#include <ros/ros.h>
+#include <rosbag/bag.h>
+#include <rosbag/view.h>
+
+#include <visualization_msgs/Marker.h>
+#include <std_msgs/String.h>
+#include <sensor_msgs/PointCloud2.h>
+
+#include <eigen3/Eigen/Eigen>
+#include <eigen3/Eigen/Dense>
+
+
+#include <pcl/io/pcd_io.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl_ros/point_cloud.h>
+#include <pcl/point_types.h>
+
+typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
+
+using namespace Eigen;
+using namespace std;
+
+//// Variables used in this file ////
+
+// point cloud data
+string scene_filename, map_filename;
+PointCloud::Ptr cloud_scene (new PointCloud);
+PointCloud::Ptr cloud_map (new PointCloud);
+
+
+// ros publisher
+ros::Publisher m_pub_scene, m_pub_map;
+
+void _load_pcd() {
+    scene_filename = "/home/kevin/catkin_ws/src/scene.pcd";
+    map_filename = "/home/kevin/catkin_ws/src/map.pcd";
+
+    if(pcl::io::loadPCDFile<pcl::PointXYZ> (scene_filename, *cloud_scene) == -1) {
+        PCL_ERROR("Could not real file %s", scene_filename);
+    }
+    if(pcl::io::loadPCDFile<pcl::PointXYZ> (map_filename, *cloud_map) == -1) {
+        PCL_ERROR("Could not real file %s", map_filename);
+    }
+    cout << "Load PCDs success" << endl;
+}
+
+void _publish() {
+    
+    //cloud_scene->header.frame_id = "/map";
+    //cloud_scene->header.stamp = ros::Time::now().toNSec();
+    //m_pub.publish(cloud_scene);
+    //cout << "Point cloud publish success" << endl;
+    //ros::spinOnce();
+    
+    //pcl_conversions::fromPCL(cloud_scene, output);
+
+    //m_pub.publish(output);
+
+}
+
+int main(int argc, char** argv) {
+    // initialize ros
+    ros::init(argc, argv, "pcl");
+    ros::NodeHandle n;
+
+    // for publishing pcl information
+    m_pub_scene = n.advertise<PointCloud> ("output_scene", 1);
+    m_pub_map = n.advertise<PointCloud> ("output_map", 1);
+
+    _load_pcd();
+
+    // publish point cloud
+    cloud_scene->header.frame_id = "/map";
+    cloud_map->header.frame_id = "/map";
+
+    ros::Rate loop_rate(4);
+
+    while(n.ok()) {
+        pcl_conversions::toPCL(ros::Time::now(), cloud_scene->header.stamp);
+        pcl_conversions::toPCL(ros::Time::now(), cloud_scene->header.stamp);
+        m_pub_scene.publish(cloud_scene);
+        m_pub_map.publish(cloud_map);
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+
+
+    //ros::spin();
+
+    return 0;
+}
+
